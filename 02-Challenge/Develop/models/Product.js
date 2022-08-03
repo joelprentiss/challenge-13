b269114f17e78e0ad8595d1ed2ca6d0572792bd2
Joel Prentiss <joel.prentiss89@gmail.com>
2022-08-02 21:40:51 -0500

    finished 2 models

--- 02-Challenge/Develop/models/Product.js
+++ 02-Challenge/Develop/models/Product.js
@@ -2,6 +2,8 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
+const ProductTag = require('./ProductTag');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,6 +12,34 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    prodcutName:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    catagoryId:{
+     type: DataTypes.INTEGER,
+     references:{
+      model: Category,
+      key: 'id'
+     } 
+    },
+    productTag:{
+      type: DataTypes.INTEGER,
+      references:{
+        model: ProductTag,
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
